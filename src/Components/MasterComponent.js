69ffb8f2eb4da7013687887847bf39765fa0dbe6
unknown <lokeshram122@gmail.com>
2022-03-11 14:47:55 +0530

    third commit

--- src/Components/MasterComponent.js
+++ src/Components/MasterComponent.js
@@ -7,6 +7,7 @@ const [FirstName,setFirstName]  = useState("");
 const [LastName,setLastName]  = useState("");
 const [Name,setName] = useState([]);
 const [mode,setmode] = useState("New");   
+const [editid,seteditid] = useState(0);
 
 
 let onclickEvents=(e)=>{
@@ -23,9 +24,19 @@ else if(e.target.name == "LastName")
   
 }
 
+let guid = () => {
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
 let onSubmit =()=>{
     var array = Name;
-    array.push({ID:array.length+1,FirstName:FirstName,LastName:LastName})
+    array.push({ID:guid(),FirstName:FirstName,LastName:LastName})
     setName(array)
     setFirstName("")
     setLastName("");
@@ -51,16 +62,29 @@ let onedit=(Id)=>{
     console.log(array)
     setFirstName(array[0].FirstName)
     setLastName(array[0].LastName)
+    seteditid(array[0].ID)
     setmode("Edit")
 }
 
 let onEditSubmit=()=>{
     var array = Name;
-    array.push({ID:array.length+1,FirstName:FirstName,LastName:LastName})
-    setName(array)
+    //array[editid].FirstName=FirstName
+    //array[editid].LastName=LastName
+    console.log(array,editid)
+    debugger;
+    array = array.forEach(element => {
+        if(element.ID == editid)
+        {
+            element.FirstName=FirstName
+            element.LastName=LastName
+        }
+    });
+    console.log(array)
+    //setName(array)
+    // setName(array)
     setFirstName("")
     setLastName("");
-    console.log(Name)
+    //console.log(Name)
     setmode("New")
 }
 
@@ -68,19 +92,18 @@ let onEditSubmit=()=>{
 
 
 return (<div>
-        <div className="w100 fleft mt10">
+        <div className="w100 fleft mt10 ml10">
         <span className="fleft mr10">First Name</span><input className="fleft" type="text" name="FirstName" value={FirstName} onChange={(e)=>{onclickEvents(e)}}></input>
         </div>
-        <div className="w100 fleft mt10">
+        <div className="w100 fleft mt10 ml10">
         <span className="fleft mr10">Last Name</span><input className="fleft" type="text" name="LastName" value={LastName} onChange={(e)=>{onclickEvents(e)}}></input>
         </div>
-        <div className="fleft w100 mt10">
+        <div className="fleft w100 mt10 ml190">
         {mode=="New"?<button className="fleft" type="button" onClick={onSubmit}>Submit</button>
-        :<button className="fleft" type="button" onClick={onEditSubmit}>Submit</button>}
+        :<button className="fleft" type="button" id={editid} onClick={onEditSubmit}>Edit</button>}
         </div>
         <table>
         <tr>
-                <th>ID</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Edit</th>
@@ -89,7 +112,7 @@ return (<div>
         <tbody>
         {Name.map((n)=>{
             return(
-                <tr><td>{n.ID}</td><td>{n.FirstName}</td><td>{n.LastName}</td><td><button onClick={()=>ondelete(n.ID)}>Delete</button></td><td><button onClick={()=>onedit(n.ID)}>Edit</button></td></tr>
+                <tr><td>{n.FirstName}</td><td>{n.LastName}</td><td><button className="btn btn-default btn-sm yellow" onClick={()=>onedit(n.ID)}><span class="glyphicon glyphicon-pencil"></span> Edit</button></td><td><button className="btn btn-default btn-sm red" onClick={()=>ondelete(n.ID)}><span class="glyphicon glyphicon-trash"></span> Delete</button></td></tr>
             )
         })}
         </tbody>    
