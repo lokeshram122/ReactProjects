9be66000d3fd5f3ea8b6d18b38519486e64edc4b
LOKESH RAM <lokeshram122@gmail.com>
2022-03-14 14:49:03 +0530

    final commit 5

--- src/Components/MasterComponent.js
+++ src/Components/MasterComponent.js
@@ -1,4 +1,8 @@
 import {React,useEffect,useState} from "react";
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
+import ReactTooltip from 'react-tooltip';
+
+
 const axios = require('axios')
 
 
@@ -9,7 +13,9 @@ function Master(props){
 
 const [FirstName,setFirstName]  = useState("");
 const [LastName,setLastName]  = useState("");
-const [gender,setgender]  = useState("");
+const [gender,setgender]  = useState("Male");
+const [country,setcountry]  = useState("");
+const [region,setregion]  = useState("");
 const [Name,setName] = useState([]);
 const [mode,setmode] = useState("New");   
 const [editid,seteditid] = useState(0);
@@ -37,7 +43,6 @@ const getApiData = async () => {
   }, []);
    
     
-
 
 let onclickEvents=(e)=>{
 if(e.target.name=="FirstName")
@@ -73,7 +78,11 @@ myHeaders.append("Content-Type", "application/json");
 var raw = JSON.stringify({
   "first_name": FirstName,
   "last_name": LastName,
-  "gender":gender
+  "gender":gender,
+  "address":{
+      "country":country,
+      "region":region
+  }
 });
 
 var requestOptions = {
@@ -90,7 +99,10 @@ var response = await fetch("http://localhost:4000/register", requestOptions)
   .then(result => id=result)
   .catch(error => console.log('error', error));
 
-  array.push({_id:id,first_name:FirstName,last_name:LastName,gender:gender})
+  array.push({_id:id,first_name:FirstName,last_name:LastName,gender:gender,address:{
+      country:country,
+      region:region
+  }})
 
 
     setName(array)
@@ -131,6 +143,8 @@ let onedit=(Id)=>{
     setFirstName(array[0].first_name)
     setLastName(array[0].last_name)
     setgender(array[0].gender)
+    setcountry(array[0].address.country)
+    setregion(array[0].address.region)
     seteditid(array[0]._id)
     setmode("Edit")
 }
@@ -147,7 +161,11 @@ let onEditSubmit=async()=>{
     var raw = JSON.stringify({
       "first_name": FirstName,
       "last_name": LastName,
-      "gender":gender
+      "gender":gender,
+      "address":{
+        "country":country,
+        "region":region
+    }
     });
     
     var requestOptions = {
@@ -170,6 +188,8 @@ let onEditSubmit=async()=>{
             element.first_name=FirstName
             element.last_name=LastName
             element.gender=gender
+            element.address.country = country
+            element.address.region = region
         }
     });
     console.log(array)
@@ -177,6 +197,9 @@ let onEditSubmit=async()=>{
     // setName(array)
     setFirstName("")
     setLastName("");
+    setgender("Male")
+    setcountry("")
+    setregion("")
     //console.log(Name)
     setmode("New")
 }
@@ -184,38 +207,57 @@ let onEditSubmit=async()=>{
 
 
 
-return (<div>
-        <div className="w100 fleft mt10 ml10">
-        <span className="fleft mr10">First Name</span><input className="fleft" type="text" name="FirstName" value={FirstName} onChange={(e)=>{onclickEvents(e)}}></input>
+return (<div className="w50 fleft">
+        <div className="w100 fleft mt10">
+        <span className="fleft" >First Name :</span>
+        <input className="fright"  type="text" name="FirstName" value={FirstName} onChange={(e)=>{onclickEvents(e)}}></input>
         </div>
-        <div className="w100 fleft mt10 ml10">
-        <span className="fleft mr10">Last Name</span><input className="fleft" type="text" name="LastName" value={LastName} onChange={(e)=>{onclickEvents(e)}}></input>
+        <div className="w100 fleft mt10" >
+        <span className="fleft">Last Name :</span><input className="fright"  type="text" name="LastName" value={LastName} onChange={(e)=>{onclickEvents(e)}}></input>
         </div>
-        <div className="w100 fleft mt10 ml10">
-        <input className="fleft mr10" type="radio" value="Male" name="gender" checked={gender=="Male"}  onChange={(e)=>setgender(e.target.value)} /> <span className="fleft mr10" >  Male</span>
-        <input className="fleft mr10" type="radio" value="Female" name="gender" checked={gender=="Female"}  onChange={(e)=>setgender(e.target.value)}/> <span className="fleft mr10" >  Female</span>
-        <input className="fleft mr10" type="radio" value="Other" name="gender" checked={gender=="Other"}  onChange={(e)=>setgender(e.target.value)}/> <span className="fleft mr10" >  Other</span>
+        <div className="w100 fleft mt10" >
+        <span className="fleft" >Gender :</span>
+        <input className="fright" type="radio" value="Other" name="gender" checked={gender=="Other"}  onChange={(e)=>setgender(e.target.value)}/>   <span className="fright mr10 ml10" >  Other </span>
+        <input className="fright" type="radio" value="Female" name="gender" checked={gender=="Female"}  onChange={(e)=>setgender(e.target.value)}/> <span className="fright ml10 mr10" >  Female </span>
+         <input className="fright"  type="radio" value="Male" name="gender" checked={gender=="Male"}  onChange={(e)=>setgender(e.target.value)} /> <span className="fright ml10 mr10">  Male </span>
         </div>
-        <div className="fleft w100 mt10 ml190">
-        {mode=="New"?<button className="fleft" type="button" onClick={onSubmit}>Submit</button>
-        :<button className="fleft" style={{marginLeft:"23px"}} type="button" id={editid} onClick={onEditSubmit}>Edit</button>}
+        <div className="w100 fleft mt10">
+        <span  className="fleft">Country :</span><CountryDropdown 
+          className="fright"
+          value={country}
+          onChange={(val) => setcountry(val)} />
+        </div>
+        <div className="w100 fleft mt10">
+        <span className="fleft" >Region :</span><RegionDropdown 
+         className="fright"
+          country={country}
+          value={region}
+          onChange={(val) => setregion(val)} />
+        </div>
+        <div className="w100 fleft mt10">
+        {mode=="New"?<button className="fright" type="button" onClick={onSubmit}>Submit</button>
+        :<button className="fright" type="button" id={editid} onClick={onEditSubmit}>Edit</button>}
         </div>
         <table>
         <tr>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Gender</th>
+                <th>Country</th>
                 <th>Edit</th>
                 <th>Delete</th>
         </tr>
         <tbody>
         {Name.map((n)=>{
             return(
-                <tr><td>{n.first_name}</td><td>{n.last_name}</td><td>{n.gender}</td><td><button className="btn btn-default btn-sm yellow" onClick={()=>onedit(n._id)}><span class="glyphicon glyphicon-pencil"></span> Edit</button></td><td><button className="btn btn-default btn-sm red" onClick={()=>ondelete(n._id)}><span class="glyphicon glyphicon-trash"></span> Delete</button></td></tr>
+                <tr><td>{n.first_name}</td><td>{n.last_name}</td><td>{n.gender}</td>
+                <td data-tip={n.address.region}>{n.address.country}</td><td><button className="btn btn-default btn-sm yellow" onClick={()=>onedit(n._id)}><span class="glyphicon glyphicon-pencil"></span> Edit</button></td><td><button className="btn btn-default btn-sm red" onClick={()=>ondelete(n._id)}><span class="glyphicon glyphicon-trash"></span> Delete</button></td></tr>
             )
         })}
+        <ReactTooltip />
         </tbody>    
         </table> 
+        
     </div>)
 }
 
